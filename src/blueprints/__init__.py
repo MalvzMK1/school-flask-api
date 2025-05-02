c1e87d182405d9eac39fad5be83e1284c93fa90f
Matheus Alves de Oliveira <matheus.adeoliveira11@gmail.com>
2025-05-02 16:14:50 -0300

    feat: add namespaces to API

--- src/blueprints/__init__.py
+++ src/blueprints/__init__.py
@@ -1,3 +1,3 @@
-from .students import student_bp
-from .teachers import teacher_bp
-from .course_class import course_class_bp
+from .students import student_ns
+from .teachers import teacher_ns
+from .course_class import course_class_ns
